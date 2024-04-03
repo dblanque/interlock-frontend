415ca08e74f42d8049ab7e9f529efe8ffb5943c1
dblanque <dylan.blanque@gmail.com>
2024-04-03 19:52:27 -0300

    Compile new dist

--- components.d.ts
+++ components.d.ts
@@ -100,7 +100,6 @@ declare module 'vue' {
     VRow: typeof import('vuetify/lib')['VRow']
     VSelect: typeof import('vuetify/lib')['VSelect']
     VSlideXReverseTransition: typeof import('vuetify/lib')['VSlideXReverseTransition']
-    VSlideXTransition: typeof import('vuetify/lib')['VSlideXTransition']
     VSlideYReverseTransition: typeof import('vuetify/lib')['VSlideYReverseTransition']
     VSlideYTransition: typeof import('vuetify/lib')['VSlideYTransition']
     VSnackbar: typeof import('vuetify/lib')['VSnackbar']
