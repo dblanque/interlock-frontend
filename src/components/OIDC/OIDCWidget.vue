f81daf1c8ff694f26e821a85f962245c9f874886
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-11 13:13:40 -0300

    Visual updates.

--- src/components/OIDC/OIDCWidget.vue
+++ src/components/OIDC/OIDCWidget.vue
@@ -5,17 +5,12 @@
 	<!-- @change OK with TOTP -->
 	<!-- @change cancel -->
 	 <v-row>
-		<v-col cols="12" class="ma-0 pa-0 mt-2">
-			<p class="font-weight-medium mb-0">
-				{{ $tc("classes.application", 1) }}
-			</p>
-		</v-col>
 		<v-col cols="12" class="ma-0 pa-0 my-4">
 			<h1>
 				{{ applicationName }}
 			</h1>
 		</v-col>
-		<v-col cols="12" class="ma-0 pa-0">
+		<v-col cols="12" class="ma-0 pa-0 mb-6">
 			<p class="ma-0 pa-0">{{ $t("section.login.oidcRequestedByApp") }}</p>
 		</v-col>
 	 </v-row>
