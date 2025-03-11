6f577055132d30650aba943ddf98d585e9790f20
Dylan Blanque <dylan.blanque@gmail.com>
2025-03-11 13:13:30 -0300

    Fix error code numeric conversion.

--- src/plugins/mixin/utilsMixin.js
+++ src/plugins/mixin/utilsMixin.js
@@ -3,9 +3,9 @@
 // File: utilsMixin.js
 
 const utilsMixin = {
-    methods:{
+    methods: {
         toCamelCase(e) {
-            return e.replace(/_([a-z])/g, (g) =>  g[1].toUpperCase());
+            return e.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
         },
         toSnakeCase(e) {
             if (e.toLowerCase() === e) {
@@ -15,7 +15,7 @@ const utilsMixin = {
                 (str, c) => str.replace(new RegExp(c), '_' + c.toLowerCase()),
                 e
             )
-            .substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0);
+                .substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0);
         },
         toPropCase(e) {
             if (e.toLowerCase() === e) {
@@ -25,12 +25,12 @@ const utilsMixin = {
                 (str, c) => str.replace(new RegExp(c), '-' + c.toLowerCase()),
                 e
             )
-            .substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0);
+                .substring((e.slice(0, 1).match(/([A-Z])/g)) ? 1 : 0);
         },
         // ----------------------------- LDAP Permissions ----------------------------- //
         calcEnabledPermissions(permissionList) {
             let result = 0
-			if (!permissionList) return result
+            if (!permissionList) return result
             for (const [key] of Object.entries(permissionList)) {
                 if (permissionList[key].value == true) {
                     result += permissionList[key].int
@@ -39,27 +39,32 @@ const utilsMixin = {
             return result
         },
         getEnabledPermissions(permissionList) {
-			let result = []
-			if (!permissionList) return result
+            let result = []
+            if (!permissionList) return result
             for (const [key] of Object.entries(permissionList)) {
-				if (!(key in permissionList)) {
-					console.error(`${key} not in Permission list.`)
-					continue
-				}
+                if (!(key in permissionList)) {
+                    console.error(`${key} not in Permission list.`)
+                    continue
+                }
                 if (permissionList[key].value == true)
                     result.push(key)
             }
             return result
         },
         // ---------------------------------------------------------------------------- //
-        arraysAreEqual: function(a, b, sort) {
+        arraysAreEqual: function (a, b, sort) {
             let a_clone = structuredClone(a)
             if (sort == true) a_clone.sort()
             let b_clone = structuredClone(b)
             if (sort == true) b_clone.sort()
             return JSON.stringify(a_clone) === JSON.stringify(b_clone)
         },
-        isNumber: function(evt) {
+        isNumeric(str) {
+            if (typeof str != "string") return false // we only process strings!  
+            return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+                !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+        },
+        isNumber: function (evt) {
             // TODO - Analyze Clipboard data besides standard keypress events
             evt = (evt) ? evt : window.event;
             let charCode = (evt.which) ? evt.which : evt.keyCode;
@@ -85,11 +90,11 @@ const utilsMixin = {
                     errorData.response.data != undefined &&
                     errorData.response.data != null &&
                     errorData.response.data instanceof Object) {
-                        if ('ldap_response' in errorData.response.data)
-                            codeToUse = errorData.response.data.ldap_response.description
-                        else if ('code' in errorData.response.data)
-                            codeToUse = errorData.response.data.code
-                    }
+                    if ('ldap_response' in errorData.response.data)
+                        codeToUse = errorData.response.data.ldap_response.description
+                    else if ('code' in errorData.response.data)
+                        codeToUse = errorData.response.data.code
+                }
                 else if ('code' in errorData)
                     codeToUse = errorData.code
                 else if ('status_code' in errorData)
@@ -98,13 +103,13 @@ const utilsMixin = {
                     codeToUse = errorData.response.status
             }
 
-            return(codeToUse)
+            return (codeToUse)
         },
-        getMessageForCode(error){
+        getMessageForCode(error) {
             let codeToUse = this.getResponseErrorCode(error)
             let suffix
 
-            if(codeToUse != undefined && codeToUse != null){
+            if (codeToUse != undefined && codeToUse != null) {
                 if (typeof codeToUse !== 'string')
                     codeToUse = toString(codeToUse)
 
@@ -115,83 +120,82 @@ const utilsMixin = {
             }
 
             // Attempt to cast to numeric
-            try {
+            if (this.isNumeric(codeToUse))
                 codeToUse = Number(codeToUse)
-            } catch (error) {}
             // OTP ---------------------------------------------------------- //
             if (/^otp_.*/.test(codeToUse))
-                return this.$t('error.codes.otp.'+codeToUse)
+                return this.$t('error.codes.otp.' + codeToUse)
             // DNS ---------------------------------------------------------- //
             else if (/^dns_.*/.test(codeToUse))
-                return this.$t('error.codes.dns.'+codeToUse)
+                return this.$t('error.codes.dns.' + codeToUse)
             else if (/^user_.*/.test(codeToUse))
-                return this.$t('error.codes.users.'+codeToUse)
+                return this.$t('error.codes.users.' + codeToUse)
             else if (/^ldap_.*/.test(codeToUse))
-                return this.$t('error.codes.ldap.'+codeToUse)
+                return this.$t('error.codes.ldap.' + codeToUse)
             else if (/^group_.*/.test(codeToUse))
-                return this.$t('error.codes.groups.'+codeToUse)
+                return this.$t('error.codes.groups.' + codeToUse)
             else if (/^setting_.*/.test(codeToUse))
-                return this.$t('error.codes.settings.'+codeToUse)
+                return this.$t('error.codes.settings.' + codeToUse)
             else if (/^application_.*/.test(codeToUse))
-                return this.$t('error.codes.applications.'+codeToUse)
+                return this.$t('error.codes.applications.' + codeToUse)
             else if (/^oidc_.*/.test(codeToUse))
-                return this.$t('error.codes.oidc.'+codeToUse)
+                return this.$t('error.codes.oidc.' + codeToUse)
             else
-                switch(codeToUse){
-                case 'ERR_UNKNOWN':
-                    return this.$t("error.unknown")
-                case 'ERR_UNKNOWN_SHORT':
-                    return this.$t("error.unknown_short")
-                case 'ERR_LDAP_GW':
-                    return this.$t('error.codes.ldapGwError')
-                case 400:
-                case 'bad_request':
-                case 'ERR_BAD_REQUEST':
-                case 'data_key_missing':
-                    return this.$t('error.codes.400')
-                case 403:
-                case 'permission_denied':
-                    return this.$t('error.codes.403')
-                case 406:
-                case 'not_acceptable':
-                    return this.$t('error.codes.406')
-                case 500:
-                case 'ERR_SERV_ERROR':
-                case 'ERR_BAD_RESPONSE':
-                    return this.$t('error.codes.500')
-                case 'access_token_invalid':
-                case 'refresh_token_expired':
-                case 'unauthorized':
-                    return this.$t('error.codes.401')
-                case 'ERR_INVALID_CSV':
-                    return this.$t('error.codes.invalidCSV')
-                case 'ERR_INVALID_CSV_HEADERS':
-                    return this.$t('error.codes.invalidCSVHeaders')
-                case 'ERR_FILEREADER':
-                    return this.$t('error.codes.fileReader')
-                case 409:
-                    return this.$t('error.codes.conflict')
-                case 'ERR_NETWORK':
-                    return this.$t('error.codes.networkError')
-                case 'namingViolation':
-                    return this.$t('error.codes.namingViolation')
-                // Users -------------------------------------------------------- //
-                case 'noUsersInImport':
-                    return this.$t('error.codes.users.noUsersInImport')
-                // -------------------------------------------------------------- //
-                case null:
-                case undefined:
-                case "":
-                default:
-                    console.log("Unknown Error Code: "+codeToUse)
-                    if (codeToUse in this.$t('error.codes'))
-                        return this.$t('error.codes.'+codeToUse)
-                    else {
-                        var msg = this.$t("error.unknown_short")
-                        if (suffix != undefined && suffix != null && codeToUse != msg)
-                            msg = msg + suffix
-                        return msg
-                    }
+                switch (codeToUse) {
+                    case 'ERR_UNKNOWN':
+                        return this.$t("error.unknown")
+                    case 'ERR_UNKNOWN_SHORT':
+                        return this.$t("error.unknown_short")
+                    case 'ERR_LDAP_GW':
+                        return this.$t('error.codes.ldapGwError')
+                    case 400:
+                    case 'bad_request':
+                    case 'ERR_BAD_REQUEST':
+                    case 'data_key_missing':
+                        return this.$t('error.codes.400')
+                    case 403:
+                    case 'permission_denied':
+                        return this.$t('error.codes.403')
+                    case 406:
+                    case 'not_acceptable':
+                        return this.$t('error.codes.406')
+                    case 500:
+                    case 'ERR_SERV_ERROR':
+                    case 'ERR_BAD_RESPONSE':
+                        return this.$t('error.codes.500')
+                    case 'access_token_invalid':
+                    case 'refresh_token_expired':
+                    case 'unauthorized':
+                        return this.$t('error.codes.401')
+                    case 'ERR_INVALID_CSV':
+                        return this.$t('error.codes.invalidCSV')
+                    case 'ERR_INVALID_CSV_HEADERS':
+                        return this.$t('error.codes.invalidCSVHeaders')
+                    case 'ERR_FILEREADER':
+                        return this.$t('error.codes.fileReader')
+                    case 409:
+                        return this.$t('error.codes.conflict')
+                    case 'ERR_NETWORK':
+                        return this.$t('error.codes.networkError')
+                    case 'namingViolation':
+                        return this.$t('error.codes.namingViolation')
+                    // Users -------------------------------------------------------- //
+                    case 'noUsersInImport':
+                        return this.$t('error.codes.users.noUsersInImport')
+                    // -------------------------------------------------------------- //
+                    case null:
+                    case undefined:
+                    case "":
+                    default:
+                        console.log("Unknown Error Code: " + codeToUse)
+                        if (codeToUse in this.$t('error.codes'))
+                            return this.$t('error.codes.' + codeToUse)
+                        else {
+                            var msg = this.$t("error.unknown_short")
+                            if (suffix != undefined && suffix != null && codeToUse != msg)
+                                msg = msg + suffix
+                            return msg
+                        }
                 }
         },
 
@@ -203,20 +207,20 @@ const utilsMixin = {
         },
 
         sortNullLast(items, index, isDesc) {
-        items.sort((a, b) => {
-            if (a[index] === b[index]) { // equal items sort equally
-            return 0;
-            } else if (a[index] === null || a[index] === '') { // nulls sort after anything else
-            return 1;
-            } else if (b[index] === null || b[index] === '') {
-            return -1;
-            } else if (!isDesc[0]) { // otherwise, if we're ascending, lowest sorts first
-            return a[index] < b[index] ? -1 : 1;
-            } else { // if descending, highest sorts first
-            return a[index] < b[index] ? 1 : -1;
-            }
-        });
-        return items;
+            items.sort((a, b) => {
+                if (a[index] === b[index]) { // equal items sort equally
+                    return 0;
+                } else if (a[index] === null || a[index] === '') { // nulls sort after anything else
+                    return 1;
+                } else if (b[index] === null || b[index] === '') {
+                    return -1;
+                } else if (!isDesc[0]) { // otherwise, if we're ascending, lowest sorts first
+                    return a[index] < b[index] ? -1 : 1;
+                } else { // if descending, highest sorts first
+                    return a[index] < b[index] ? 1 : -1;
+                }
+            });
+            return items;
         }
     }
 }
